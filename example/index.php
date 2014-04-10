6f0e2168f7a5c691a99d68bbdcaac9b45a5522f0
ehunter-usgs <ehunter@usgs.gov>
2014-04-10 08:29:14 -0600

    Increase font size and line height responsively.

--- example/index.php
+++ example/index.php
@@ -12,6 +12,7 @@ if (!isset($TEMPLATE)) {
 ?>
 
 
+<<<<<<< HEAD
 <p>
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -147,3 +148,95 @@ if (!isset($TEMPLATE)) {
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
+=======
+<h2 class="underline">Font Sizes</h2>
+
+<h1>First Header</h1>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisici<strong>*</strong> g elit,
+sed do eius<strong>*</strong> od tempor<strong>*</strong> incididunt ut labore
+et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+deserunt mollit anim id est laborum.
+</p>
+
+<h2>Second Header</h2>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisici<strong>*</strong> g elit,
+sed do eius<strong>*</strong> od tempor<strong>*</strong> incididunt ut labore
+et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+deserunt mollit anim id est laborum.
+</p>
+
+<h3>Third Header</h3>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisici<strong>*</strong> g elit,
+sed do eius<strong>*</strong> od tempor<strong>*</strong> incididunt ut labore
+et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+deserunt mollit anim id est laborum.
+</p>
+
+<h4>Fourth Header</h4>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisici<strong>*</strong> g elit,
+sed do eius<strong>*</strong> od tempor<strong>*</strong> incididunt ut labore
+et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+deserunt mollit anim id est laborum.
+</p>
+
+<h5>Fifth Header</h5>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisici<strong>*</strong> g elit,
+sed do eius<strong>*</strong> od tempor<strong>*</strong> incididunt ut labore
+et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+deserunt mollit anim id est laborum.
+</p>
+
+
+
+<h2 class="underline">Headers</h2>
+
+
+<h1>First Header</h1>
+<h2>Second Header</h2>
+<h3>Third Header</h3>
+<h4>Fourth Header</h4>
+<h5>Fifth Header</h5>
+
+<h1 class="thin">First Header</h1>
+<h2 class="thin">Second Header</h2>
+<h3 class="thin">Third Header</h3>
+<h4 class="thin">Fourth Header</h4>
+<h5 class="thin">Fifth Header</h5>
+
+<h1 class="subheader">First Header</h1>
+<h2 class="subheader">Second Header</h2>
+<h3 class="subheader">Third Header</h3>
+<h4 class="subheader">Fourth Header</h4>
+<h5 class="subheader">Fifth Header</h5>
+
+<h1 class="underline">First Header</h1>
+<h2 class="underline">Second Header</h2>
+<h3 class="underline">Third Header</h3>
+<h4 class="underline">Fourth Header</h4>
+<h5 class="underline">Fifth Header</h5>
+
+
+
+<h2>Lists</h2>
+
+>>>>>>> Increase font size and line height responsively.
