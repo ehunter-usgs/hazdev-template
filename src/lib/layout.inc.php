48b6ec4b75de9a353cd9fd016b6707220add21b3
ehunter-usgs <ehunter@usgs.gov>
2017-08-01 17:02:09 -0600

    Add styles to allow the width of a page to be constrained

--- src/lib/layout.inc.php
+++ src/lib/layout.inc.php
@@ -16,113 +16,127 @@
 <body>
 
   <header role="banner" class="site-header">
-    <a class="site-logo" href="/" title="U.S. Geological Survey">
-      <img src="/theme/images/usgs-logo.svg" alt=""/>
-    </a>
-    <a class="jumplink-navigation" href="#site-sectionnav">Jump to Navigation</a>
-    <?php
-
-      if ($COOPERATORS) {
-        echo '<div class="cooperators">' .
-            $COOPERATORS .
-            '</div>';
-      }
-
-    ?>
-  </header>
-
-  <main role="main" class="page" aria-labelledby="page-header">
-    <header class="page-header" id="page-header">
-      <h1><?php echo $TITLE; ?></h1>
-    </header>
-
-    <div class="page-content">
+    <div class="header-content">
+      <a class="site-logo" href="/" title="U.S. Geological Survey">
+      <img class="desktop-logo" src="/theme/images/usgs-logo.svg" alt=""/>
+      <img class="mobile-logo" src="/theme/images/usgs-logo-no-text.svg" alt=""/>
+      </a>
+      <a class="jumplink-navigation" href="#site-sectionnav">Jump to Navigation</a>
       <?php
 
-        // setup include guard
-        $TEMPLATE = true;
-        // include original page for content
-        include($_SERVER['SCRIPT_FILENAME']);
+        if ($COOPERATORS) {
+          echo '<div class="cooperators">' .
+              $COOPERATORS .
+              '</div>';
+        }
 
       ?>
     </div>
+  </header>
+  <div class="banner">
+    <div class="banner-content"></div>
+  </div>
 
-    <?php
+  <main role="main" aria-labelledby="page-header">
+    <div class="main-content">
+      <section class="page" >
+        <header class="page-header" id="page-header">
+          <h1><?php echo $TITLE; ?></h1>
+        </header>
 
-      if ($CONTACT_URL || $SOCIAL) {
+        <div class="page-content">
+          <?php
 
-        echo '<footer class="page-footer">';
+            // setup include guard
+            $TEMPLATE = true;
+            // include original page for content
+            include($_SERVER['SCRIPT_FILENAME']);
 
-        if ($CONTACT_URL) {
-          echo '<p><a href="' .
-              str_replace('{CONTACT}', $CONTACT, $CONTACT_URL) .
-              '">Questions or comments?</a></p>';
-        }
+          ?>
+        </div>
 
-        if ($SOCIAL) {
-          $pageUrl = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'Off')
-              || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
-              $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) ?
-              'https://' : 'http://' . $_SERVER['HTTP_HOST'] .
-              $_SERVER['REQUEST_URI'];
+        <?php
 
-          $replacements = array(
-              '{URL}' => htmlspecialchars($pageUrl),
-              '{TITLE}' => htmlspecialchars($TITLE));
-          $urlReplacements = array(
-              '{URL}' => urlencode($pageUrl),
-              '{TITLE}' => urlencode($TITLE));
+          if ($CONTACT_URL || $SOCIAL) {
 
-          echo '<nav class="page-social" aria-label="Share this page">';
-          foreach ($SOCIAL as $link) {
-            echo ' <a' .
-                ' href="' . htmlspecialchars(strtr($link['url'], $urlReplacements)) . '"' .
-                ' title="Share using ' . htmlspecialchars($link['name']) . '"' .
-                ' class="' . $link['class'] . '"' .
-                ' data-link-template="' . htmlspecialchars($link['url']) . '"' .
-                '>' . htmlspecialchars($link['name']) . '</a>';
+            echo '<footer class="page-footer">';
+
+            if ($CONTACT_URL) {
+              echo '<p><a href="' .
+                  str_replace('{CONTACT}', $CONTACT, $CONTACT_URL) .
+                  '">Questions or comments?</a></p>';
+            }
+
+            if ($SOCIAL) {
+              $pageUrl = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'Off')
+                  || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+                  $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) ?
+                  'https://' : 'http://' . $_SERVER['HTTP_HOST'] .
+                  $_SERVER['REQUEST_URI'];
+
+              $replacements = array(
+                  '{URL}' => htmlspecialchars($pageUrl),
+                  '{TITLE}' => htmlspecialchars($TITLE));
+              $urlReplacements = array(
+                  '{URL}' => urlencode($pageUrl),
+                  '{TITLE}' => urlencode($TITLE));
+
+              echo '<nav class="page-social" aria-label="Share this page">';
+              foreach ($SOCIAL as $link) {
+                echo ' <a' .
+                    ' href="' . htmlspecialchars(strtr($link['url'], $urlReplacements)) . '"' .
+                    ' title="Share using ' . htmlspecialchars($link['name']) . '"' .
+                    ' class="' . $link['class'] . '"' .
+                    ' data-link-template="' . htmlspecialchars($link['url']) . '"' .
+                    '>' . htmlspecialchars($link['name']) . '</a>';
+              }
+              echo '</nav>';
+            }
+
+            echo '</footer>';
           }
-          echo '</nav>';
-        }
 
-        echo '</footer>';
+        ?>
+      </section>
 
-      }
+      <nav class="site-footer">
+        <?php
 
-    ?>
+          if ($NAVIGATION) {
+            echo '<section' .
+                ' id="site-sectionnav"' .
+                ' class="site-sectionnav"' .
+                ' aria-label="Section Navigation"' .
+                '>' . $NAVIGATION . '</section>';
+          }
+
+          if ($SITE_SITENAV) {
+            echo '<section class="site-sitenav" aria-label="Site Navigation">' .
+                $SITE_SITENAV . '</section>';
+          }
+
+        ?>
+
+        <form class="site-search" role="search" action="//search.usa.gov/search" method="get" accept-charset="UTF-8">
+          <input name="utf8" type="hidden" value="✓"/>
+          <input name="affiliate" type="hidden" value="usgs"/>
+          <input name="sitelimit" type="hidden" value="<?php echo $SITE_URL; ?>"/>
+          <input id="query" name="query" type="search" placeholder="Search..." title="Search"/>
+          <button type="submit">Search</button>
+        </form>
+      </nav>
+
+    </div>
   </main>
-
-  <nav class="site-footer">
-    <?php
-
-      if ($NAVIGATION) {
-        echo '<section' .
-            ' id="site-sectionnav"' .
-            ' class="site-sectionnav"' .
-            ' aria-label="Section Navigation"' .
-            '>' . $NAVIGATION . '</section>';
-      }
-
-      if ($SITE_SITENAV) {
-        echo '<section class="site-sitenav" aria-label="Site Navigation">' .
-            $SITE_SITENAV . '</section>';
-      }
-
-    ?>
-
-    <form class="site-search" role="search" action="//search.usa.gov/search" method="get" accept-charset="UTF-8">
-      <input name="utf8" type="hidden" value="✓"/>
-      <input name="affiliate" type="hidden" value="usgs"/>
-      <input name="sitelimit" type="hidden" value="<?php echo $SITE_URL; ?>"/>
-      <input id="query" name="query" type="search" placeholder="Search..." title="Search"/>
-      <button type="submit">Search</button>
-    </form>
-  </nav>
 
   <?php
 
     if ($SITE_COMMONNAV) {
-      echo '<footer class="site-commonnav">' . $SITE_COMMONNAV . '</footer>';
+      echo '<footer class="site-commonnav">' .
+          '<div class="footer-content"' .
+            $SITE_COMMONNAV .
+          '</div' .
+        '</footer>';
     }
 
     // IE <= 9 polyfills
